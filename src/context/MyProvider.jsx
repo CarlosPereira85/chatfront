7e95b4ae7e7155361f05e40af67f763d36694afe
CarlosPereira85 <caiqueberlin@gmail.com>
2023-02-13 04:53:43 +0100

    first commit

--- src/context/MyProvider.jsx
+++ src/context/MyProvider.jsx
@@ -86,7 +86,7 @@ const MyProvider = ({ children }) => {
       
    
       
-      const response = await fetch('http://localhost:5000/', {
+      const response = await fetch('https://gptback.onrender.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
